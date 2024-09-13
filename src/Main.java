8d167e314fff9cd93356371b3bc1c6518443c044
Franco Ignacio Rivas Vidal <francorivasvidal01@gmail.com>
2024-09-13 18:29:02 -0300

    segundo commit con mi nombre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String args) {
-        System.out.println("Hola mi nombre es Franco");
+        System.out.println("Hola mi nombre es Franco Rivas");
     }
 }
