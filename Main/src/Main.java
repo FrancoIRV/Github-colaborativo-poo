6656d4625a6458509de03cb7485c523c9b984398
HectorRosales <h.rosales01@ufromail.cl>
2024-09-13 15:48:50 -0300

    Progama hola mundo + mensaje con el nombre de hector.

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
+        System.out.printf("Mensaje enviado por: Hector Rosales");
     }
 }
