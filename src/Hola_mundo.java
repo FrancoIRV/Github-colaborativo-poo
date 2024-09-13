2ff24b1dc8b93b7030663ba935334db518f37936
SolidSnake-0 <d.araneda05@ufromail.cl>
2024-09-13 18:36:03 -0300

    Parte 2

--- src/Hola_mundo.java
+++ src/Hola_mundo.java
@@ -1,6 +1,7 @@
 public class Hola_mundo {
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
+        System.out.printf("Mensaje enviado por: Diego Araneda");
 
     }
 }
